e3a129efc07822a33cafd2ee6edfebe091b971be
Josh West <josh.west@centre.edu>
2019-09-19 11:04:17 -0400

    finished

--- scrape.js
+++ scrape.js
@@ -1,5 +1,14 @@
 var scraper = require('./tools').scraper;
 //scraper = function(url,parseFcn,filename)
 //scrapes a URL, parses it with parseFcn, and archives the data in a JSON file called filename.
-
-
+var getChars = function($,data)
+{
+    var parent = $(".az-list");
+    var children = parent.find("li");
+    children.toArray()
+    .forEach(function(item,index)
+     {
+        data.push($(item).text());
+    });
+}
+scraper("https://www.marvel.com/comics/characters",getChars,"character.jsonp");
